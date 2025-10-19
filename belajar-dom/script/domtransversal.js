9b3ecea0661fb885538a528d95c81fc8bcefed27
diazabdi257 <diazabdi2003@gmail.com>
2025-10-19 14:58:16 +0700

    ss

---
+++ belajar-dom/script/domtransversal.js
@@ -0,0 +1,13 @@
+const close = document.querySelectorAll(".close");
+
+// for (let i = 0; i < close.length; i++) {
+//   close[i].addEventListener("click", function (e) {
+//     // close[i].parentElement.style.display = 'none';
+//     // atau bisa (tapi menambahkan params e)
+//     e.target.parentElement.style.display ='none';
+//   });
+// };
+
+close.forEach(el => {
+    el.addEvent
+});
